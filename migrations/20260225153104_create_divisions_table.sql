73bcb0e5a3c7d20cb291d8c83c1d58d5d265eca4
Riyan Amanda <ryant.n92@gmail.com>
2026-02-27 22:42:13 +0700

    refactor: add is_active to category and division db schema

--- migrations/20260225153104_create_divisions_table.sql
+++ migrations/20260225153104_create_divisions_table.sql
@@ -1,7 +1,9 @@
 -- +goose Up
 CREATE TABLE divisions (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
+    name VARCHAR(50) NOT NULL UNIQUE,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- +goose Down
